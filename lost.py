5171aadb6da029a07d99f9d39dad8da68543b154
Vaishali1219 <46643151+Vaishali1219@users.noreply.github.com>
2019-04-27 14:54:27 +0530

    Create lost.py

---
+++ lost.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'lost.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog_lost(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(332, 119)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(-10, -10, 351, 91))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.OK1 = QtWidgets.QPushButton(Dialog)
+        self.OK1.setGeometry(QtCore.QRect(120, 70, 75, 23))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.OK1.setFont(font)
+        self.OK1.setObjectName("OK1")
+
+        self.retranslateUi(Dialog)
+        self.OK1.clicked.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Sorry OOps Try again:("))
+        self.OK1.setText(_translate("Dialog", "OK"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog_lost()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
